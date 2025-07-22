852d9dc2d286a5bc326091cb60f4094737266265
Érick Souza de Melo <analuiisar@gmail.com>
2025-07-21 21:03:05 -0300

    task finalizada

--- AgendaBarbearia.js
+++ AgendaBarbearia.js
@@ -1,5 +1,6 @@
 
 // JavaScript para input select //
+// Escurece a cor do texto quando o corte for selecionado. 
 document.addEventListener('DOMContentLoaded', function () {
   const select = document.getElementById('corte');
 
@@ -24,29 +25,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
 const formulario = document.getElementById('formulario');
 const tabela = document.getElementById('tabela').querySelector('tbody');
-const agenda = [];
+const agenda = []; /*criar objeto*/ 
 
 formulario.addEventListener('submit', function (evento) {
-  evento.preventDefault();
+  evento.preventDefault(); /* Impede o botão submit de recarregar a página*/ 
 
   const nome = document.getElementById('Nome').value;
   const telefone = document.getElementById('Telefone').value;
   const email = document.getElementById('Email').value;
   const corte = document.getElementById('corte').value;
   const data = document.getElementById('Data').value;
-  const horario = document.getElementById('Horário').value;
+  const horario = document.getElementById('Horario').value;
 
-  if (corte === "Buzz Cut") {
-    console.log(nome + " escolheu corte Buzz Cut.");
-  } else if (corte === "Fade/Degradê") {
-    console.log(nome + " escolheu corte Fade/Degradê");
-  } else if (corte === "Mullet Moderno") {
-    console.log(nome + " escolheu o corte Mullet Moderno");
-  } else if (corte === "Old Money") {
-    console.log(nome + " escolheu o corte Old Money");
-  }
 
-  const agendamento = { nome, telefone, email, corte, data, horario };
+  const agendamento = { nome, telefone, email, corte, data, horario }; /* criar uma lista*/ 
 
   let conflito = false;
   for (let i = 0; i < agenda.length; i++) {
