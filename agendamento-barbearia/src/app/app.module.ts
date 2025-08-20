0aefcfdc84d02f416e1c02ddf9f8b1a46c327a48
Érick Souza de Melo <analuiisar@gmail.com>
2025-08-20 14:49:37 -0300

    adicionando angular

--- agendamento-barbearia/src/app/app.module.ts
+++ agendamento-barbearia/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServicosComponent } from './servicos/servicos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicosComponent
   ],
   imports: [
     BrowserModule,
